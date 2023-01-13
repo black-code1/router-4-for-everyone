d77469c53d2edcc1d92bdd5fba89bcb8a4569a28
black-code1 <legrandmahamat1@gmail.com>
2023-01-13 10:49:52 +0100

    Adding Routes and Content

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,16 @@
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
   </div>
 
-  <br>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 
-  <router-view></router-view>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
