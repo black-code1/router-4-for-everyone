ad419fd11aa25760f388d2091e913322ba0a564c
black-code1 <legrandmahamat1@gmail.com>
2023-01-14 00:11:40 +0100

    Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <the-navigation/>
   <div class="container">
-    <router-view :key="$route.path"></router-view>
+    <router-view v-slot="{Component}">
+<!--      <transition name="slide" mode="out-in">-->
+<!--      <transition name="moveUp">-->
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
+
   </div>
 
 </template>
@@ -13,3 +20,43 @@ export default {
   components: {TheNavigation}
 }
 </script>
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+/*.moveUp-enter-active {*/
+/*  animation: fadeIn 1s ease-in;*/
+/*}*/
+
+/*@keyframes fadeIn {*/
+/*    0% {opacity: 0;}*/
+/*    50% {opacity: 0.5;}*/
+/*    100% {opacity: 1;}*/
+/*}*/
+
+/*.moveUp-leave-active {*/
+/*  animation: moveUp 0.3s ease-in;*/
+/*}*/
+
+/*@keyframes moveUp {*/
+/*  0% {transform: translateY(0);}*/
+/*  100% {transform: translateY(-400px);}*/
+/*}*/
+/*  .slide-enter-active,*/
+/*  .slide-leave-active{*/
+/*    transition: opacity 1s, transform 1s;*/
+/*  }*/
+
+/*  .slide-enter-from,*/
+/*  .slide-leave-to{*/
+/*    opacity: 0;*/
+/*    transform: translateX(-30%);*/
+/*  }*/
+</style>
